7bd97540110221bfbc8896e466f8f07236a3c0d5
EmelVv <vmemelianov0906@gmail.com>
2023-08-11 00:07:07 +0300

    save

---
+++ OpenGL/Object.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <string>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+#include "Texture.hpp"
+
+
+namespace Engine {
+
+
+}
